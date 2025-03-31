c8fa3d44bb7d3c266532a3f0ee096397163b850a
Talan Espandju <tespandj@bess-f2r5s15.clusters.42paris.fr>
2025-03-31 05:22:22 +0200

    check map is valid working

--- srcs/parsing/mapping.c
+++ srcs/parsing/mapping.c
@@ -17,6 +17,65 @@ static	void	next_line(t_cub *cub, char **line)
 	*line = gnl(cub->fd);
 }
 
+/// @brief checker qu'il ne soit pas sur une extremite de la map
+/// checker que y - 1 ne contiennent pas d'espaces sur x - 1 | x | x + 1
+/// checker que y + 1 ne contiennent pas d'espaces sur x - 1 | x | x + 1
+/// checker que y  ne contiennent pas d'espaces sur x - 1 | x + 1
+static	bool	validcase(char **matrix, int width, int x, int y)
+{
+	if (y == 0 || x == 0 || x == tstrlen(matrix[y]) - 1 || y == width)  // check ligne du dessus
+		return (false);
+	if (y - 1 >= 0) // check ligne du haut
+	{
+		if (tstrlen(matrix[y - 1]) <= x + 1) // ligne contienne 3 characteres au dessus de x
+			return (false);
+		if (x - 1 >= 0)
+			if (matrix[y - 1][x - 1] == ' ')
+				return (false);
+		if (matrix[y - 1][x] == ' ')
+			return (false);
+		if (matrix[y - 1][x + 1] == ' ')
+			return (false);
+	}
+	if (y + 1 <= width) // check ligne du bas
+	{
+		if (tstrlen(matrix[y + 1]) <= x + 1) // ligne contienne 3 characteres au dessus de x
+			return (false);
+		if (x - 1 >= 0)
+			if (matrix[y + 1][x - 1] == ' ')
+				return (false);
+		if (matrix[y + 1][x] == ' ')
+			return (false);
+		if (matrix[y + 1][x + 1] == ' ')
+			return (false);
+	}
+	if (x + 1 <= tstrlen(matrix[y])) // check ligne actuelle
+	{
+		if (matrix[y][x - 1] == ' ')
+			return (false);
+		if (matrix[y][x + 1] == ' ')
+			return (false);
+	}
+	return (true);
+
+}
+
+static	bool	validmap(t_cub *cub, char **matrix)
+{
+	int	x;
+	int	y;
+
+	y = -1;
+	while (matrix[++y])
+	{
+		x = -1;
+		while (matrix[y][++x])
+			if (matrix[y][x] == '0' && !validcase(matrix, cub->map->L, x, y))
+				return (false);
+	}
+	return (true);
+}
+
 void	mapping(t_cub *cub, char *save, char *line)
 {
 	int	width = 0;
@@ -27,6 +86,7 @@ void	mapping(t_cub *cub, char *save, char *line)
 	while (!null_line(line))
 	{
 		++width;
+		///// corriger la ligne si elle comprend des tab sinon segfault
 		if (tstrlen(line) > cub->map->l)
 			cub->map->l = tstrlen(line);
 		while (null_line(line))
@@ -43,10 +103,10 @@ void	mapping(t_cub *cub, char *save, char *line)
 		next_line(cub, &line);
 	}
 	cub->map->matrix = split(save, '\n');
+	cub->map->L = width;
 	free(save);
-	if (!cub->map->matrix)
-		wgas(cub, "split matrix failed", NULL);
+	if (!cub->map->matrix || !validmap(cub, cub->map->matrix))
+		wgas(cub, "map invalid", NULL);
 	if (cub->fd > 0)
 		close(cub->fd);
-	cub->map->L = width;
 }
